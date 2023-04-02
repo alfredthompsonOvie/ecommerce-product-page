c5f3e48c8286fa0d4a53ca9ce3613f9004a722dd
thompson <alfredthompsonovie@gmail.com>
2023-04-03 00:18:30 +0100

    product section

--- js/app.js
+++ js/app.js
@@ -4,21 +4,25 @@ const closeMenu = document.querySelector(".closeMenu");
 const navList = document.querySelector(".nav__list");
 const next = document.querySelector(".next");
 const previous = document.querySelector(".previous");
-const thumbnailsContainer = document.querySelector(".thumbnailsContainer");
-const showcaseImg = document.querySelector(".showcase--img");
+const thumbnailsContainer = document.querySelector(".product__img__thumbnails");
+const thumbnails = document.querySelectorAll(".thumbnail");
+const thumbnailWrapper = document.querySelectorAll(".product__img__thumbnail");
+const showcaseImg = document.querySelector(".showcase");
+
+const counter = document.querySelector(".counter");
+const btnMinus = document.querySelector(".btn__minus");
+const btnPlus = document.querySelector(".btn__plus");
 
 openMenu.addEventListener("click", () => {
-  openMenu.classList.add("hide")
-  closeMenu.classList.remove("hide");
-  navList.classList.remove("hide");
+	openMenu.classList.add("hide");
+	closeMenu.classList.remove("hide");
+	navList.classList.remove("hide");
 });
 closeMenu.addEventListener("click", () => {
-  openMenu.classList.remove("hide")
-  closeMenu.classList.add("hide");
-  navList.classList.add("hide");
+	openMenu.classList.remove("hide");
+	closeMenu.classList.add("hide");
+	navList.classList.add("hide");
 });
-
-
 
 // !Gallery
 const gallery = [
@@ -35,26 +39,67 @@ const gallery = [
 // ];
 
 let currentIndex = 0;
-// console.log(gallery.length);
-next.addEventListener('click', () => {
-  if (currentIndex === gallery.length - 1) {
-    currentIndex = -1
+// console.log(gallery.length);\
+
+next.addEventListener("click", () => {
+	if (currentIndex === gallery.length - 1) {
+		currentIndex = -1;
+	}
+	currentIndex++;
+	showcaseImg.setAttribute("src", gallery[currentIndex]);
+});
+previous.addEventListener("click", () => {
+	if (currentIndex === 0) {
+		currentIndex = gallery.length;
+	}
+	currentIndex--;
+	showcaseImg.setAttribute("src", gallery[currentIndex]);
+});
+
+// thumbnails.forEach(thumbnail => {
+//   let thumbnailIdx = +thumbnail.dataset.index 
+//   if (thumbnailIdx === currentIndex) {
+//     console.log(thumbnailIdx);
+//     console.log(currentIndex);
+//     thumbnail.classList.add('activeImg');
+
+//   }
+// })
+thumbnailsContainer.addEventListener("click", (e) => {
+  // const target = e.target.closest('.thumbnail');
+  const target = e.target.closest('.product__img__thumbnail');
+	if (!target) return;
+	removeActive();
+	target.classList.add('is--active');
+	// currentImg.src = gallery[+target.dataset.index];
+	// currentIndex = +target.dataset.index;
+
+	showcaseImg.setAttribute("src", gallery[+e.target.dataset.index]);
+  currentIndex = +e.target.dataset.index;
+  // console.log(target);
+	// console.log("currIdx", currentIndex);
+});
+const removeActive= ()=>{
+  // thumbnails.forEach(thumbnail => thumbnail.classList.remove('is--active')
+  // )
+  thumbnailWrapper.forEach(thumbnail => thumbnail.classList.remove('is--active')
+  )
   }
-  currentIndex++
-  showcaseImg.setAttribute('src', gallery[currentIndex]);
-})
-previous.addEventListener('click', () => {
-  if (currentIndex === 0) {
-    currentIndex = gallery.length
-  }
-  currentIndex--
-  showcaseImg.setAttribute('src', gallery[currentIndex]);
-})
-thumbnailsContainer.addEventListener('click', (e) => {
-  showcaseImg.setAttribute('src', gallery[+e.target.dataset.index]);
-  currentIndex = +e.target.dataset.index
-  console.log("currIdx", currentIndex);
-})
 // thumbnails.forEach(tn=> tn.addEventListener("click", (e) => {
 //   console.log(e);
 // }))
+
+// ---------------------------------------------
+let productQuantity = 0;
+counter.textContent = productQuantity;
+btnMinus.addEventListener("click", () => {
+	if (productQuantity === 0) {
+		return 0;
+	}
+	productQuantity--;
+	counter.textContent = productQuantity;
+});
+btnPlus.addEventListener("click", () => {
+	productQuantity++;
+	counter.textContent = productQuantity;
+});
